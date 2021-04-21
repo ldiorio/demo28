c7a5d2211eda46849ecbe95d4e00bd4946804454
ldiorio <lisa.diorio@gmail.com>
2021-04-21 15:22:31 -0400

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var port = process.env.PORT || 3000;
+//var port = 8080;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write ("<h2>Node js demo</h2>");
   res.write("Hello World - how's it going?<br>");
   res.end();
-}).listen(8080);
+}).listen(port);
