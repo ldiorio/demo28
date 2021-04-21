ceeef060d5717c4fcce1565619b824f924b2f223
ldiorio <lisa.diorio@gmail.com>
2021-04-21 19:19:07 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -6,6 +6,6 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write ("<h2>Node js demo</h2>");
   res.write("Hello World - how's it going?<br>");
-  res.write("This is working online!<br>");
+  res.write("This is now working online!<br>");
   res.end();
 }).listen(port);
